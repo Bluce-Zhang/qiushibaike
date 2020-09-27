e03946fe629cfe4d6e082feffebb4c1bdcedcca9
xiaolong <andzxland@163.com>
2020-09-28 00:17:06 +0800

    分装话题详情组件

---
+++ components/topic-detail/topic-info.vue
@@ -0,0 +1,84 @@
+<template>
+	<view>
+		<!-- 背景图 -->
+		<view class="bg position-relative">
+			<image class="image filter" :src="info.titlepic" mode="scaleToFill"></image>
+		</view>
+		
+		<!-- 中部头像和描述 -->
+		<view class="middle position-relative">
+			<view class="common-flex head-image">
+				<image :src="info.titlepic"></image>
+				<text>#{{info.title}}#</text>
+			</view>
+			<view class="common-flex common-flex-align-items info">
+				<text>动态:{{info.totalnum}}</text>
+				<text>今日:{{info.todaynum}}</text>
+			</view>
+			<view class="describe">
+				<text>{{info.desc}}</text>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		props:['info']
+	}
+	
+</script>
+
+<style lang="scss">
+	.image {
+		width: 100%;
+		height: 300rpx;
+	}
+	
+	/**
+	 * 图片模糊效果
+	 */
+	.filter {
+		filter: blur(3px);
+	}
+	
+	.middle {
+		z-index: 10;
+		background-color: #FFFFFF;
+		padding: 0rpx 20rpx;
+	
+		.head-image {
+			image {
+				height: 150rpx;
+				width: 150rpx;
+				border-radius: 5px;
+				margin-top: -75rpx;
+			}
+	
+			text {
+				margin-left: 20rpx;
+			}
+		}
+	
+		.info,
+		.describe {
+			color: #666666;
+		}
+	
+		.info {
+			margin-top: 10rpx;
+	
+			text {
+				margin: 0rpx 10rpx;
+			}
+		}
+	
+		.describe {
+			text {
+				margin: 0rpx 10rpx;
+			}
+		}
+	}
+</style>
--- pages/topic-detail/topic-detail.vue
+++ pages/topic-detail/topic-detail.vue
@@ -1,93 +1,45 @@
 <!-- 话题详情页面 -->
 <template>
 	<view>
-		<!-- 背景图 -->
-		<view class="bg position-relative">
-			<image class="image filter" src="../../static/demo/datapic/16.jpg" mode="scaleToFill"></image>
-		</view>
-		
-		<!-- 中部头像和描述 -->
-		<view class="middle position-relative">
-			<view class="common-flex head-image">
-				<image src="../../static/demo/datapic/16.jpg"></image>
-				<text>#话题标题#</text>
-			</view>
-			<view class="common-flex common-flex-align-items info">
-				<text>动态:10</text>
-				<text>今日:0</text>
-			</view>
-			<view class="describe">
-				<text>话题描述</text>
-			</view>
-			
-		</view>
+		<topic-info :info="info"></topic-info>
+		<!-- 分割线 -->
+		<view class="divider"></view>
 	</view>
 </template>
 
 <script>
+	import topicInfo from '@/components/topic-detail/topic-info.vue'
 	export default {
+		components:{
+			topicInfo
+		},
 		data() {
 			return {
-				
+				info: {
+					"title": "淘宝记录簿",
+					"titlepic": "../../static/demo/topicpic/13.jpeg",
+					"desc": "120斤到85斤 我的反转人生",
+					"totalnum": 20,
+					"todaynum": 15
+				}
+
 			}
 		},
 		onLoad(e) {
-			if(e.detail){
-				let res = JSON.stringify(e.detail);
-				console.log(res);
+			if (e.detail) {
+				let res = JSON.parse(e.detail);
+				console.log(JSON.stringify(res));
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style lang="scss">
-
-	.image{
-		width: 100%;
-		height: 300rpx;
+	.divider{
+		height: 20rpx;
+		background-color: #EEEEEE;
 	}
-	/**
-	 * 图片模糊效果
-	 */
-	.filter{
-		filter: blur(3px);
-	}
-	
-	.middle{
-		z-index: 10;
-		background-color: #FFFFFF;
-		padding: 0rpx 20rpx;
-		.head-image{
-			image{ 
-				height: 150rpx;
-				width: 150rpx;
-				border-radius: 5px;
-				margin-top: -75rpx;
-			}
-			text{
-				margin-left: 20rpx;
-			}
-		}
-		
-		.info,.describe{
-			color: #666666;
-		}
-		
-		.info{
-			margin-top: 10rpx;
-			text{
-				margin: 0rpx 10rpx;
-			}
-		}
-		
-		.describe{
-			text{
-				margin: 0rpx 10rpx;
-			}
-		}
-	}
-
 </style>
